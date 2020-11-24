83fdd91d6775acd593fc53df94f3375707b0e03f
Ajay Rao <ajayrao@MacBook-Pro-97.local>
2020-11-24 13:19:30 -0500

    working on tooltip

--- js/Stacked_Bar_Chart.js
+++ js/Stacked_Bar_Chart.js
@@ -141,10 +141,13 @@ function stacked_bar_chart() {
 			.domain(["wt axon", "wt a+d", "dlk-1 a+d"]);
 
 		// tooltip
-		d3.select('body')
-			.append('div')
-			.attr('id', 'tooltip')
-			.attr('style', 'position: absolute; opacity: 0;');
+		let tooltip = d3.select('body')
+			.append("div")
+			.style("position", "absolute")
+			.style("z-index", "10")
+			.style("visibility", "hidden")
+			.style("background", "#000")
+			.text("a simple tooltip");
 		
 		
 
@@ -175,23 +178,19 @@ function stacked_bar_chart() {
 				.attr("width",40)
 				.style("stroke", "black")
 				.attr("class", classify)
-				.on('mouseover', function(d) {
-					d3.select('#tooltip').transition().duration(200).style('opacity', 1).text("hello")
-				})
-				.on('mouseout', function(d) {
-					d3.select('#tooltip').style('opacity', 0)
-				})
 				//.on('mousemove', function(d) {
 					//d3.select('#tooltip')
 					//	.style('left', (d3.event.pageX+10) + 'px')
 					//	.style('top', (d3.event.pageY+10) + 'px')
 			//	})
-
 				.on("mouseover", highlight)
 				// deselect dots and color black upon mouseout
 				.on("mouseout", function (d) {
 				  d3.select(this).classed("selected", false) 
-			});
+			})
+				.on("mouseover", function(d){tooltip.text("hello"); return tooltip.style("visibility", "visible");})
+				//.on("mousemove", function(){return tooltip.style("top", (d3.event.pageY-10)+"px").style("left",(d3.event.pageX+10)+"px");})
+				.on("mouseout", function(){return tooltip.style("visibility", "hidden");});
 			
 				
 					
